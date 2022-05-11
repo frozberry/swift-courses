d345ade31f772dd9e5cdc155a378ca11d43f9ec4
frozberry <93860959+frozberry@users.noreply.github.com>
2022-05-11 20:42:54 +0100

    Remove users post route

--- src/pages/api/users/users.ts
+++ src/pages/api/users/users.ts
@@ -1,19 +1,7 @@
 import { User } from "@prisma/client"
 import type { NextApiRequest, NextApiResponse } from "next"
 import authAdminSession from "../../../lib/authAdminSession"
-import { ServerError } from "../../../lib/types"
-import {
-  createUser,
-  findUserByEmail,
-  getUsers,
-  toggleUserField,
-} from "../../../services/server/userService"
-
-type PostBody = {
-  parentName: string
-  email: string
-  password: string
-}
+import { getUsers, toggleUserField } from "../../../services/server/userService"
 
 const GET = async (req: NextApiRequest, res: NextApiResponse<User[]>) => {
   const { unauthorized, response } = await authAdminSession(req, res)
@@ -21,25 +9,6 @@ const GET = async (req: NextApiRequest, res: NextApiResponse<User[]>) => {
 
   const users = await getUsers()
   res.send(users)
-}
-
-const POST = async (
-  req: NextApiRequest,
-  res: NextApiResponse<string | ServerError>
-) => {
-  const { parentName, email, password }: PostBody = req.body
-
-  const existingUser = await findUserByEmail(email)
-
-  if (existingUser) {
-    return res.status(400).send({
-      type: "userAlreadyExists",
-      message: "Account already exists, maybe you meant to log in?",
-    })
-  }
-
-  const user = await createUser(parentName, email, password)
-  res.send(user)
 }
 
 type PutProps = {
@@ -61,9 +30,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case "GET":
       GET(req, res)
-      break
-    case "POST":
-      POST(req, res)
       break
     case "PUT":
       PUT(req, res)
