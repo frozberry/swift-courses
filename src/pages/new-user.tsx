73f2a2a9777071017b11edd7f0fbf60fa3a636f9
frozberry <93860959+frozberry@users.noreply.github.com>
2022-05-11 17:04:38 +0100

    Style new user page

--- src/pages/new-user.tsx
+++ src/pages/new-user.tsx
@@ -2,6 +2,7 @@ import { Box, Container, LinearProgress, Typography } from "@mui/material"
 import { useRouter } from "next/router"
 import { useState } from "react"
 import NewPasswordForm from "../components/forms/NewPasswordForm"
+import Image from "next/image"
 
 const Page = () => {
   const router = useRouter()
@@ -11,9 +12,17 @@ const Page = () => {
   return (
     <>
       {loading && <LinearProgress />}
-      <Container maxWidth="xs">
+      <Container maxWidth="xs" sx={{ mt: 4 }}>
+        <Typography variant="h3" sx={{ mb: 2, textAlign: "center" }}>
+          Congratulations on enrolling!
+        </Typography>
+        <Box sx={{ textAlign: "center" }}>
+          <Image width={256} height={256} alt="Welcome" src="/medal.png" />
+        </Box>
         <Box sx={{ my: 4 }}>
-          <Typography variant="h2">Set your password</Typography>
+          <Typography variant="h2" sx={{ textAlign: "center" }}>
+            Set your password to continue to your account
+          </Typography>
         </Box>
 
         <NewPasswordForm setLoading={setLoading} sessionId={session} />
