2071fe6f9a1d16233ec78241898b9456db01bd70
frozberry <93860959+frozberry@users.noreply.github.com>
2022-05-12 21:28:15 +0100

    Comment out seed

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,67 +1,67 @@
-import fs from "fs"
-import { prisma } from "./client"
+// import fs from "fs"
+// import { prisma } from "./client"
 
-const deleteAll = async () => {
-  await prisma.user.deleteMany()
-}
+// const deleteAll = async () => {
+//   await prisma.user.deleteMany()
+// }
 
-const createUsers = async () => {
-  await prisma.user.create({
-    data: {
-      id: "3fa5a38b-e8fc-41a7-95d0-04f438ec3ff3",
-      email: "pannicope@gmail.com",
-      name: "Henry",
-      admin: true,
-      passwordHash:
-        "$2b$10$7f2Zo6SqU6E7r./LmGDkSOsQNSpcGTRiIKHpx/.ZK0G7sxTXskXsq",
-      stripeId: "cus_JifnHyNvFpbJIx",
-      ff: true,
-      pp: false,
-      kotc: false,
-    },
-  })
-}
+// const createUsers = async () => {
+//   await prisma.user.create({
+//     data: {
+//       id: "3fa5a38b-e8fc-41a7-95d0-04f438ec3ff3",
+//       email: "pannicope@gmail.com",
+//       name: "Henry",
+//       admin: true,
+//       passwordHash:
+//         "$2b$10$7f2Zo6SqU6E7r./LmGDkSOsQNSpcGTRiIKHpx/.ZK0G7sxTXskXsq",
+//       stripeId: "cus_JifnHyNvFpbJIx",
+//       ff: true,
+//       pp: false,
+//       kotc: false,
+//     },
+//   })
+// }
 
-const main = async () => {
-  const args = process.argv.slice(2)
-  const reset = args.includes("--reset")
+// const main = async () => {
+//   const args = process.argv.slice(2)
+//   const reset = args.includes("--reset")
 
-  if (reset) {
-    console.log("Resetting database")
-    await deleteAll()
-    return
-  }
+//   if (reset) {
+//     console.log("Resetting database")
+//     await deleteAll()
+//     return
+//   }
 
-  console.log("Seeding database")
+//   console.log("Seeding database")
 
-  await deleteAll()
-  // await createUsers()
+//   await deleteAll()
+//   // await createUsers()
 
-  const json = fs.readFileSync("../../temp/users/old.json", "utf8")
-  const old = JSON.parse(json)
+//   const json = fs.readFileSync("../../temp/users/old.json", "utf8")
+//   const old = JSON.parse(json)
 
-  console.log(old[0])
-  console.log(old.length)
+//   console.log(old[0])
+//   console.log(old.length)
 
-  const n = old.map((item: any) => ({
-    name: item.name,
-    email: item.email,
-    passwordHash: item.passwordHash,
-    ff: item.footworkFastlane,
-    pp: item.powerPathway,
-    kotc: item.kotc,
-    admin: true,
-  }))
+//   const n = old.map((item: any) => ({
+//     name: item.name,
+//     email: item.email,
+//     passwordHash: item.passwordHash,
+//     ff: item.footworkFastlane,
+//     pp: item.powerPathway,
+//     kotc: item.kotc,
+//     admin: true,
+//   }))
 
-  await prisma.user.createMany({ data: n })
-  await prisma.user.update({
-    where: { email: "pannicope@gmail.com" },
-    data: {
-      admin: true,
-    },
-  })
-}
+//   await prisma.user.createMany({ data: n })
+//   await prisma.user.update({
+//     where: { email: "pannicope@gmail.com" },
+//     data: {
+//       admin: true,
+//     },
+//   })
+// }
 
-main().catch((e) => {
-  throw e
-})
+// main().catch((e) => {
+//   throw e
+// })
