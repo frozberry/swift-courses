43de779f9cba2cd604849bb61c8d25c165501c73
frozberry <93860959+frozberry@users.noreply.github.com>
2022-05-12 15:46:09 +0100

    Redirect if logged in

--- src/pages/new-user.tsx
+++ src/pages/new-user.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { useReward } from "react-rewards"
 import NewPasswordForm from "../components/forms/NewPasswordForm"
+import { useSession } from "../hooks/useSession"
+import { MySession } from "../lib/types"
 
 const Page = () => {
   const router = useRouter()
@@ -15,10 +17,14 @@ const Page = () => {
     decay: 0.96,
     elementCount: 50,
   })
-
+  const { session } = useSession() as { session: MySession }
   useEffect(() => {
     reward()
   })
+
+  if (session) {
+    router.push("/")
+  }
 
   return (
     <>
