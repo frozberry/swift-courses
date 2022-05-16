a4195f2360f256cda8555dccb33e8f6853e5fd9b
frozberry <93860959+frozberry@users.noreply.github.com>
2022-05-16 14:36:36 +0100

    Add session checking back

--- src/pages/api/users/self.ts
+++ src/pages/api/users/self.ts
@@ -13,6 +13,11 @@ const GET = async (req: NextApiRequest, res: NextApiResponse<User>) => {
   const country = getCountry(req)
 
   const user = await findUserById(userId)
+  // Happens when I manually change the db, and user's browswer has saved the old cookies
+  // Causing loading screen to infitely loop, with no way to sign out
+  if (!user) {
+    return res.status(200).end("Outdated session")
+  }
   await updateOnLogin(user!, country)
 
   res.send(user as User)
